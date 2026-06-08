eb91b2d16b6df53632e94b923c62101b5ac68529
lokwalinganyang <cleoekuwom96@gmail.com>
2026-06-08 20:35:37 +0300

    Temporarily bypass CSRF for admin login to debug

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.decorators.csrf import csrf_exempt
 
+admin.site.login = csrf_exempt(admin.site.login)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('enyumba.urls')),
